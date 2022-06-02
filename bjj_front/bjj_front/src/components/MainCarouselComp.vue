fb3dbbef9731253262384e83a371338cf2221571
JunOnJuly <wlgnstjd0717@gmail.com>
2022-06-02 17:12:10 +0900

    update

--- bjj_front/bjj_front/src/components/MainCarouselComp.vue
+++ bjj_front/bjj_front/src/components/MainCarouselComp.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="SelectCardDatas">get random skills</button>
+  <button id="search-button" @click="SelectCardDatas">get random skills</button>
   <div class="d-flex justify-content-center">
     <div v-for="data in CardDatas" :key="data.title" class="card mx-3" style="width: 18rem;">
       <img :src=data.thumbnail class="card-img-top">
@@ -34,5 +34,12 @@
 <style scoped>
   img {
     height: 25rem;
+  }
+
+  #search-button {
+    margin: 1% 0;
+    width: 100%;
+    border: None;
+    background-color: rgba(0, 0, 0, 0.123);
   }
 </style>
