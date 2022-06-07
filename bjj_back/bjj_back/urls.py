ed506f490dc0140a9e5dd36e8c3dad99164015e2
JunOnJuly <wlgnstjd0717@gmail.com>
2022-06-07 23:56:32 +0900

    update data

--- bjj_back/bjj_back/urls.py
+++ bjj_back/bjj_back/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/data/', include('data.urls')),
+    path('articles/', include('articles.urls')),
+    path('datas/', include('datas.urls')),
 ]
--- bjj_back/data/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-import requests
-import random
-# Create your views here.
-
-@api_view(['GET'])
